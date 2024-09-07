0b293f98cf7b5fa3665edc5379274c4a1b3be3b6
Andrew <andrewalder1234@gmail.com>
2024-09-07 12:32:49 +1200

    Fix it up and add noise.

--- simple_filter.py
+++ simple_filter.py
@@ -7,50 +7,42 @@ Date created: 7th September 2024
 
 import numpy as np
 from matplotlib import pyplot as plt
-from dataclasses import dataclass
 
-NUM_X_DATA_POINTS = 10
-NUM_Y_DATA_POINTS = 10
-
-
-@dataclass
-class Point:
-    x: float
-    y: float
-
-
-def generate_ellipse_data(semi_major_axis: float, semi_minor_axis: float):
-    """
-    Generate data for an ellipse
-    (x/a)**2 + (y/b)**2 = 1
-    :param semi_major_axis: ellipse semi major axis [m]
-    :param semi_minor_axis: ellipse semi minor axis [m]
-    :return: data representing an ellipse
-    """
-
-    x_data_points = np.linspace(-semi_major_axis, semi_major_axis, NUM_X_DATA_POINTS)
-
-    ellipse_data = []
-    for x in x_data_points:
-        y = semi_minor_axis * (1 - (x / semi_major_axis) ** 2) ** 0.5
-        # The y solution has two values, a positive and a negative
-        ellipse_data.append(Point(x=x, y=y))
-        ellipse_data.append(Point(x=x, y=-y))
-
-    return ellipse_data
-
-
-def plot_data(data_points: list) -> None:
-
-    plt.figure()
-    for point in data_points:
-        plt.scatter(x=point.x, y=point.y, color='blue', marker='x')
-    plt.show()
+NUM_DATA_POINTS = 20
 
 
 def main() -> None:
-    ellipse_data = generate_ellipse_data(semi_major_axis=5, semi_minor_axis=3)
-    plot_data(data_points=ellipse_data)
+    semi_major_axis = 5
+    semi_minor_axis = 3
+    
+    x_offset = 0
+    y_offset = 0
+
+    ellipse_data_points = []
+    t = np.linspace(0, 2 * np.pi, NUM_DATA_POINTS)
+    x = y_offset + semi_major_axis * np.cos(t)
+    y = x_offset + semi_minor_axis * np.sin(t)
+
+    plt.figure()
+    plt.title("No noise")
+    plt.plot(x, y, color='black')
+    plt.scatter(x=x, y=y, color='blue', marker='x')
+    plt.show()
+
+    # Parameters
+    mean = 0  # Mean (mu)
+    std_dev = 0.2  # Standard deviation (sigma)
+    # Generate random Gaussian numbers
+    x_noise = x + np.random.normal(mean, std_dev, NUM_DATA_POINTS)
+    y_noise = y + np.random.normal(mean, std_dev, NUM_DATA_POINTS)
+
+    plt.figure()
+    plt.title("Noise")
+    plt.plot(x, y, color='black')
+    plt.scatter(x=x_noise, y=y_noise, color='blue', marker='x')
+    plt.show()
+
+
 
 
 if __name__ == "__main__":
